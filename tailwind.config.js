40fced8518d2054db668883ac85c486705f8cb97
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-08-12 15:40:08 +0400

    Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,14 @@ module.exports = {
       primary: "var(--font-jetbrainsMono)",
     },
     extend: {
+      colors: {
+        primary: "#1c1c22",
+        accent: {
+          DEFAULT: "00ff99",
+          hover: "00e187",
+        },
+      },
+
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
