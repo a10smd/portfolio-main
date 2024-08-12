14ec81f25dd6e01f5d0727eb339266306dbe4006
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-08-12 18:50:42 +0400

    Changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,8 +3,15 @@ import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <header>
-      <div className="container mx-auto">header yeap</div>
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Abdus<span className="text-green-400">.</span>
+          </h1>
+        </Link>
+      </div>
     </header>
   );
 };
