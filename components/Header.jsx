b40851661d2bb7697108f3b878f68ba6efc9b69f
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-08-13 16:28:06 +0400

    Changes Made

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,12 +16,15 @@ const Header = () => {
         </Link>
 
         {/* desktop nav & hire me button */}
-        <div className="hidden lg:flex">
+        <div className="hidden lg:flex items-center gap-8">
           <Nav />
           <Link href="/contact">
             <Button>Hire Me</Button>
           </Link>
         </div>
+
+        {/* mobile nav */}
+        <div className="lg:hidden">Mobile nav</div>
       </div>
     </header>
   );
