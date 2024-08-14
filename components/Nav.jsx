8ec9637c94b3ec5cb52d32753db886b4cda7fea8
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-08-14 13:42:48 +0400

    Major Changes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Links = [
+  {
+    name: "home",
+    link: "/",
+  },
+  {
+    name: "services",
+    link: "/services",
+  },
+  {
+    name: "resume",
+    link: "/resume",
+  },
+  {
+    name: "work",
+    link: "/work",
+  },
+  {
+    name: "contact",
+    link: "/contact",
+  },
+];
+
 const Nav = () => {
-  return <nav>Desktop nav</nav>;
+  return (
+    <nav className="flex gap-8">
+      {Links.map((link, index) => {
+        return (
+          <Link href={link.link} key={index}>
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
 };
 
 export default Nav;
