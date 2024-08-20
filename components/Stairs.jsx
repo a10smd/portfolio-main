1687460f595e80a2afa7341b9245f6b08eb1c291
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-08-20 19:08:05 +0400

    Major Changes

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -13,8 +13,18 @@ const stairAnimation = {
   },
 };
 
+// calculate the reverse index for staggered delay
+const reverseIndex = (index) => {
+  const totalSteps = 6;
+  return totalSteps - index - 1;
+};
+
 const Stairs = () => {
-  return <div>Stairs</div>;
+  return (
+    <>
+      {/* render 6 motion divs, each representing a step of the stairs. Each div will have the same animation defined by the stairAnimation object. The delay for each div is determined by the index of the div in the array. Creating a stagger effect to animate the stairs. */}
+    </>
+  );
 };
 
 export default Stairs;
