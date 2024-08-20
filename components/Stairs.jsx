8e041f24154d7f3584d1d5c75a953430c867a730
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-08-20 18:54:40 +0400

    Major Changes

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -1,3 +1,18 @@
+import { motion } from "framer-motion";
+
+//variants
+const stairAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
+
 const Stairs = () => {
   return <div>Stairs</div>;
 };
