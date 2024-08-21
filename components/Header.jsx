20ff07b3efd234ef8e4fcf767724997380e6f002
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-08-21 16:19:35 +0400

    Major Changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,7 @@ import { Button } from "./ui/button";
 
 // components
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -24,7 +25,9 @@ const Header = () => {
         </div>
 
         {/* mobile nav */}
-        <div className="lg:hidden">Mobile nav</div>
+        <div className="lg:hidden">
+          <MobileNav />
+        </div>
       </div>
     </header>
   );
