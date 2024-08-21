e29f07f3971bcfa92fab025f927054f532209f69
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-08-21 15:40:47 +0400

    Major Changes

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -23,6 +23,23 @@ const Stairs = () => {
   return (
     <>
       {/* render 6 motion divs, each representing a step of the stairs. Each div will have the same animation defined by the stairAnimation object. The delay for each div is determined by the index of the div in the array. Creating a stagger effect to animate the stairs. */}
+      {[...Array(6)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.4,
+              ease: "easeInOut",
+              delay: reverseIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-white relative"
+          />
+        );
+      })}
     </>
   );
 };
