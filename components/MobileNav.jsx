370c2c2e0f41dc5795edd5d716fefe5ba4386240
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-08-21 16:44:52 +0400

    Major Changes

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -3,6 +3,30 @@
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
+
+const Links = [
+  {
+    name: "home",
+    link: "/",
+  },
+  {
+    name: "services",
+    link: "/services",
+  },
+  {
+    name: "resume",
+    link: "/resume",
+  },
+  {
+    name: "work",
+    link: "/work",
+  },
+  {
+    name: "contact",
+    link: "/contact",
+  },
+];
 
 const MobileNav = () => {
   return <nav>MobileNav</nav>;
