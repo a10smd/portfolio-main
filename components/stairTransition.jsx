911a99a449fd15a2e52de4b1cfed236e2c780e51
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-08-21 16:18:54 +0400

    Major Changes

--- components/stairTransition.jsx
+++ components/stairTransition.jsx
@@ -15,6 +15,14 @@ const StairTransition = () => {
         <div key={pathname}>
           <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
             <Stairs />
+            <motion.div
+              className=" h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+              initial={{ opacity: 1 }}
+              animate={{
+                opacity: 0,
+                transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+              }}
+            />
           </div>
         </div>
       </AnimatePresence>
