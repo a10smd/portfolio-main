75f5b35d8846d9424e1318f3441cd85721cec3a6
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-08-26 14:36:58 +0400

    Major Changes

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -40,7 +40,7 @@ const MobileNav = () => {
         <div className="mt-32 mb-40 text-center text-2xl">
           <Link href="/">
             <h1 className="text-4xl font-semibold">
-              Abdus<span className="text-green-400">.</span>
+              Abdus <span className="text-green-400">.</span>
             </h1>
           </Link>
         </div>
