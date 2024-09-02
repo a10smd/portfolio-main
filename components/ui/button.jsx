fc941be31b3ff70cb89ed03727dabf8747b909fd
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-02 14:46:02 +0400

    Major Changes

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -18,8 +18,7 @@ const buttonVariants = cva(
       size: {
         default: "h-[44px] px-6",
         md: "h-[48px] px-6",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        lg: "h-[56px] px-8 text-sm uppercase tracking-[2px]",
       },
     },
     defaultVariants: {
