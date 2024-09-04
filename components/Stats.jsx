15fbc8d1b6949fad99dad78476956f2840aefd6a
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-04 18:00:10 +0400

    Major Changes

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,5 +1,55 @@
-const Stats = () => {
-  return <div>Stats</div>;
-};
+"use client";
 
+import CountUp from "react-countup";
+
+const stats = [
+  {
+    num: 2,
+    text: "Years of experience",
+  },
+  {
+    num: 7,
+    text: "Project Completed",
+  },
+  {
+    num: 8,
+    text: "Technologies gained",
+  },
+  {
+    num: 158,
+    text: "Code Commits",
+  },
+];
+const Stats = () => {
+  return (
+    <section>
+      <div className="container mx-auto">
+        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+          {stats.map((item, index) => {
+            return (
+              <div
+                className="flex-1 flex gap-4 items-center justify-center xl:justify-center"
+                key={index}
+              >
+                <CountUp
+                  end={item.num}
+                  duration={5}
+                  delay={2}
+                  className="text-4xl xl:text-6xl font-extrabold"
+                />
+                <p
+                  className={`${
+                    item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
+                  }leading-snug text-white/80`}
+                >
+                  {item.text}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
 export default Stats;
