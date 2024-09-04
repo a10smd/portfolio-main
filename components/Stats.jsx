ada100a964a7e4206070dbc660c2106443defb2d
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-04 18:02:43 +0400

    Major Changes

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -22,7 +22,7 @@ const stats = [
 ];
 const Stats = () => {
   return (
-    <section>
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
       <div className="container mx-auto">
         <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
           {stats.map((item, index) => {
