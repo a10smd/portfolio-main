967feaba2cc2e2c94c833bfee2badbe89eac2f00
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-04 16:47:28 +0400

    Major Changes

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -12,13 +12,14 @@ const Photo = () => {
           transition: { delay: 2, duration: 0.4, ease: "easeIn" },
         }}
       >
+        {/* image */}
         <motion.div
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten"
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
             src="/assets/photo.png"
@@ -29,6 +30,34 @@ const Photo = () => {
             className="object-contain"
           />
         </motion.div>
+        {/* square */}
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.rect
+            x="3"
+            y="3"
+            width="500"
+            height="500"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "0 2000" }}
+            animate={{
+              strokeDasharray: ["2000 0", "0 2000"],
+              rotate: [0, 0],
+            }}
+            transition={{
+              duration: 5,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   );
