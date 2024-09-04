b748aa1d5da55ea26422d81d02e66b791aa02e3b
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-04 16:17:08 +0400

    Major Changes

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,5 +1,32 @@
+"use client";
+import { motion } from "framer-motion";
+import Image from "next/image";
 const Photo = () => {
-  return <div>Photo</div>;
+  return (
+    <div className="w-full h-full relative">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+        }}
+      >
+        <div
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-
+[498px] mix-blend-lighten"
+        >
+          <Image
+            src="/assets/photo.png"
+            priority
+            quality={100}
+            fill
+            alt="photo"
+            className="object-contain"
+          />
+        </div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default Photo;
