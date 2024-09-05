0210fc6dddc7e905ce8e1428b863d325cc142e6f
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-05 17:06:07 +0400

    Major Changes

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,5 +1,30 @@
+"Use client";
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Experienced frontend developer specializing in web development, primarily using Next.js.",
+    href: "",
+  },
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque tenetur.",
+    href: "",
+  },
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque tenetur.",
+    href: "",
+  },
+];
 const Services = () => {
-  return <div>Services page</div>;
+  return <div>services page</div>;
 };
-
 export default Services;
