2f1d99bd2a4576203f8a4557cafdca4734e0fdfb
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-10 15:58:16 +0400

    Major Changes

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,5 +1,30 @@
+"use client";
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs, // Corrected here
+} from "react-icons/fa";
+
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+
+// about data
+const about = {
+  title: "About me",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quibusdam, sunt explicabo inventore.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Luke Coleman",
+    },
+  ],
+};
+
 const Resume = () => {
-  return <div>Resume Page</div>;
+  return <div>resume page</div>;
 };
 
 export default Resume;
