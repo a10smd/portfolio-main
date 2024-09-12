2e49529cf0b3e0fa955e8ca8ab09b4245e1d5b2e
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-12 15:13:09 +0400

    Major Changes

--- components/ui/tabs.jsx
+++ components/ui/tabs.jsx
@@ -20,7 +20,7 @@ const TabsTrigger = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center w-full justify-center whitespace-nowrap rounded-lg p-3 font-medium transition-all",
+      "inline-flex items-center w-full justify-center whitespace-nowrap rounded-lg p-3 font-medium transition-all text-base",
       "bg-[#27272c] text-white", // Default background and text color
       "ring-offset-white disabled:pointer-events-none disabled:opacity-50", // Ring and disabled styles
       "data-[state=active]:bg-green-400 data-[state=active]:text-primary data-[state=active]:font-bold data-[state=active]:shadow-sm", // Active state styles
