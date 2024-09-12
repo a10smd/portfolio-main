e7f9e023471ccd38da491e6e6ecedf8b806ce389
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-12 14:23:47 +0400

    Major Changes

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -196,7 +196,7 @@ const Resume = () => {
           defaultValue="experience"
           className="flex flex-col xl:flex-row gap-[60px]"
         >
-          <TabsList>
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
             <TabsTrigger value="experience">Experience</TabsTrigger>
             <TabsTrigger value="education">Education</TabsTrigger>
             <TabsTrigger value="skills">Skills</TabsTrigger>
