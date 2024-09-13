2d5a6fa0702e9ea030a5263854fc360ec7808167
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-13 15:41:52 +0400

    Major Changes

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -19,13 +19,23 @@ import Image from "next/image";
 const projects = [
   {
     num: "01",
-    category: "Full Stack",
+    category: "fullstack",
     title: "Emirates Delight",
     description:
       "Emirates Delight is an online food delivery service that offers a variety of cuisines from all over the world. The platform was created using Next.js, React, and Tailwind CSS.",
     stack: [{ name: "Next.js" }, { name: "React" }, { name: "Tailwind CSS" }],
     live: "https://emirates-delight.vercel.app/",
     github: "https://github.com/a10smd/emirates-delight",
+  },
+  {
+    num: "02",
+    category: "frontend",
+    title: "Admin Panel",
+    description:
+      "Admin Panel is an online food delivery service that offers a variety of cuisines from all over the world. The platform was created using Next.js, React, and Tailwind CSS.",
+    stack: [{ name: "Next.js" }, { name: "React" }, { name: "Tailwind CSS" }],
+    live: "",
+    github: "https://github.com/a10smd/Admin-panel",
   },
 ];
 
