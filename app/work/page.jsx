25a88257fe3dc27e5f26bf3825c6f4bce1bee666
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-13 15:38:02 +0400

    Major Changes

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -16,6 +16,19 @@ import {
 import Link from "next/link";
 import Image from "next/image";
 
+const projects = [
+  {
+    num: "01",
+    category: "Full Stack",
+    title: "Emirates Delight",
+    description:
+      "Emirates Delight is an online food delivery service that offers a variety of cuisines from all over the world. The platform was created using Next.js, React, and Tailwind CSS.",
+    stack: [{ name: "Next.js" }, { name: "React" }, { name: "Tailwind CSS" }],
+    live: "",
+    github: "",
+  },
+];
+
 const Work = () => {
   return <div>Work</div>;
 };
