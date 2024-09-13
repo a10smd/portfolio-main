43478e336dfd6ee4bebdf9943c0df994aab33f93
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-13 15:11:28 +0400

    Major Changes

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -2,8 +2,10 @@
 
 import { motion } from "framer-motion";
 import React, { useState } from "react";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+
 import { BsArrowUpRight } from "react-icons/bs";
 import {
   Tooltip,
