fb24dee72f628c2e3ca73185365b333531cf3583
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-13 15:09:52 +0400

    Major Changes

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,3 +1,17 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { BsArrowUpRight } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+
 const Work = () => {
   return <div>Work</div>;
 };
