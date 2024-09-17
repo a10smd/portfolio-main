fe85c4979bbca054cf72a257d06e503334655ad3
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-17 14:14:41 +0400

    Major Changes

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -55,7 +55,14 @@ const Work = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-            text
+            {/* outline num */}
+            <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+              {project.num}
+            </div>
+            {/* project category */}
+            <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-green-400 transition-all duration-500 capitalize ">
+              {project.category} project
+            </h2>
           </div>
           <div className="w-full">slider</div>
         </div>
