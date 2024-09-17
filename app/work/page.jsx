5ad13cbc3dd0e9b8032d684f665fd2910f1c43a7
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-17 13:13:02 +0400

    Major Changes

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -47,8 +47,17 @@ const projects = [
 const Work = () => {
   const [project, setProject] = useState(projects[0]);
   return (
-    <motion.div>
-      <div className="container mx-auto"></div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full">text</div>
+          <div className="w-full">slider</div>
+        </div>
+      </div>
     </motion.div>
   );
 };
