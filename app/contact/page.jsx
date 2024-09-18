2b4518e0cf22d8e281f38db98157905b4f3a690e
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-18 16:20:17 +0400

    Major Changes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 const Contact = () => {
   return <div>Contact</div>;
 };
