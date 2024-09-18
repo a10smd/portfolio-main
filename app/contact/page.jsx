4f69cf95efb56c4f5d6a8ed00ea401fbd32cdeeb
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-18 17:34:49 +0400

    Major Changes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -50,7 +50,19 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
           <div className="xl:h-[54%] order-2 xl:order-none">
-            <form>form</form>
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-5xl text-green-400">Let's get in touch</h3>
+              <p className="text-white/60">
+                Fill out the form below to send me a message.
+              </p>
+              {/* input */}
+              <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+                <input type="firstname" placeholder="Firstname" />
+                <input type="lastname" placeholder="Lastname" />
+                <input type="email" placeholder="Email" />
+                <input type="phone" placeholder="Phone" />
+              </div>
+            </form>
           </div>
           {/* info */}
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
