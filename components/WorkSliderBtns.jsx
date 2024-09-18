0c7654a62e25bebdf80319703545c5e347a51157
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-18 12:23:23 +0400

    Major Changes

--- components/WorkSliderBtns.jsx
+++ components/WorkSliderBtns.jsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Swiper } from "swiper/react";
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
+
 const WorkSliderBtns = () => {
   return <div>WorkSliderBtns</div>;
 };
