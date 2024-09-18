c152687b8fa244e2a7f161a6c20254fb56d86581
AbdusSamada11d8 <147069268+AbdusSamada11d8@users.noreply.github.com>
2024-09-18 12:56:17 +0400

    Major Changes

--- components/WorkSliderBtns.jsx
+++ components/WorkSliderBtns.jsx
@@ -1,10 +1,26 @@
 "use client";
 
-import { Swiper } from "swiper/react";
+import { useSwiper } from "swiper/react";
 import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
 
-const WorkSliderBtns = () => {
-  return <div>WorkSliderBtns</div>;
+const WorkSliderBtns = ({ containerStyles, iconStyles, btnStyles }) => {
+  const swiper = useSwiper();
+  return (
+    <div className={containerStyles}>
+      <button className={btnStyles}>
+        <PiCaretLeftBold
+          className={iconStyles}
+          onClick={() => swiper.slidePrev()}
+        />
+      </button>
+      <button className={btnStyles}>
+        <PiCaretRightBold
+          className={iconStyles}
+          onClick={() => swiper.slideNext()}
+        />
+      </button>
+    </div>
+  );
 };
 
 export default WorkSliderBtns;
